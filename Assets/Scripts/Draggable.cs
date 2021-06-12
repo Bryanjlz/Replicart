0e21b623d4a4d1e66739c683ad3e5d77dd596053
Kervina <45807615+Kervina@users.noreply.github.com>
2021-06-11 20:58:35 -0400

    Update Draggable.cs

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Draggable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Draggable : MonoBehaviour {
+    private float mousePosX;
+    private float mousePosY;
+    private float displacementX;
+    private float displacementY;
+    public bool isBeingHeld = false;
+
+    private void OnMouseDown()
     {
-        
+        isBeingHeld = true;
+        displacementX = this.gameObject.transform.position.x - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).x;
+        displacementY = this.gameObject.transform.position.y - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Color current = transform.GetChild(i).GetComponent<SpriteRenderer>().color;
+            transform.GetChild(i).GetComponent<SpriteRenderer>().color = new Color(current.r, current.g, current.b, 0.3f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        isBeingHeld = false;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Color current = transform.GetChild(i).GetComponent<SpriteRenderer>().color;
+            transform.GetChild(i).GetComponent<SpriteRenderer>().color = new Color(current.r, current.g, current.b, 1f);
+        }
+    }
+
+    private void Update()
+    {
+        mousePosX = (Camera.main.ScreenToWorldPoint(Input.mousePosition)).x;
+        mousePosY = (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y;
+        if (isBeingHeld == true)
+        {
+            this.gameObject.transform.localPosition = new Vector3(mousePosX + displacementX, mousePosY + displacementY, 0);
+        } 
+
     }
 }
