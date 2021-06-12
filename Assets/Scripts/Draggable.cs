0ea91513f1a8f57dcf436d5f8bc24a015c6a89ab
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-06-11 23:24:22 -0400

    Sorting Layer Stuff fixed

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -9,11 +9,19 @@ public class Draggable : MonoBehaviour {
     private float displacementY;
     public bool isBeingHeld = false;
 
+    private Map map;
+
+    private void Start() {
+        map = GameObject.Find("Map").GetComponent<Map>();
+    }
+
     private void OnMouseDown()
     {
+        map.RemoveGroup(gameObject);
         isBeingHeld = true;
-        displacementX = this.gameObject.transform.position.x - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).x;
-        displacementY = this.gameObject.transform.position.y - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y;
+        displacementX = gameObject.transform.position.x - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).x;
+        displacementY = gameObject.transform.position.y - (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y;
+
 
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -24,6 +32,7 @@ public class Draggable : MonoBehaviour {
 
     private void OnMouseUp()
     {
+        map.AddGroup(gameObject);
         isBeingHeld = false;
 
         for (int i = 0; i < transform.childCount; i++)
@@ -39,7 +48,7 @@ public class Draggable : MonoBehaviour {
         mousePosY = (Camera.main.ScreenToWorldPoint(Input.mousePosition)).y;
         if (isBeingHeld == true)
         {
-            this.gameObject.transform.localPosition = new Vector3(mousePosX + displacementX, mousePosY + displacementY, 0);
+            gameObject.transform.localPosition = new Vector3((int)(mousePosX + displacementX), (int)(mousePosY + displacementY), 0);
         } 
 
     }
