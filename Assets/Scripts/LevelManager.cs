4db3e80d20b344ac7133182e1f1eae6c0848eb49
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2021-06-11 21:58:52 -0400

    Pause + Win

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [Header("Set in Editor")]
+    public GameObject pauseMenu;
+    public GameObject winScreen;
+    
+    [Header("Internal Values")]
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowPauseMenu() {
+        // Only display the menu for now
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void HidePauseMenu() {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Win() {
+        winScreen.SetActive(true);
+    }
+
+    public void Lose() {
+
+    }
+}
