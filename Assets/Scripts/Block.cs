eacbd8aa77e53bff8f95969dcf6ce0fe2c7f19cc
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-06-11 22:31:19 -0400

    Done border stuff Added map prefab

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -13,9 +13,14 @@ public class Block : MonoBehaviour
 
     Map map;
 
+    bool[][] surrounds;
     string spriteType;
 
     private void Awake() {
+        surrounds = new bool[3][];
+        for (int i = 0; i < 3; i++) {
+            surrounds[i] = new bool[3];
+        }
         map = GameObject.Find("Map").GetComponent<Map>();
         print(map);
         if (isSolid) {
@@ -31,31 +36,72 @@ public class Block : MonoBehaviour
 
         // Check top 3
         for (int xOff = -1; xOff < 2; xOff++) {
-            spriteType += CheckPos(xOff, 1);
+            surrounds[xOff + 1][2] = CheckPos(xOff, 1);
         }
 
         // Check middle right
-        spriteType += CheckPos(1, 0);
+        surrounds[2][1] = CheckPos(1, 0);
 
         // Check bottom 3
         for (int xOff = 1; xOff > -2; xOff--) {
-            spriteType += CheckPos(xOff, -1);
+            surrounds[xOff + 1][0] = CheckPos(xOff, -1);
         }
 
         // Check middle left
-        spriteType += CheckPos(-1, 0);
+        surrounds[0][1] = CheckPos(-1, 0);
+
+        if (surrounds[0][1] && surrounds[0][2] && surrounds[1][2]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[1][2]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[1][2] && surrounds[2][2] && surrounds[2][1]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[2][1]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[2][1] && surrounds[2][0] && surrounds[1][0]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[1][0]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[1][0] && surrounds[0][0] && surrounds[0][1]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
+        if (surrounds[0][1]) {
+            spriteType += "0";
+        } else {
+            spriteType += "1";
+        }
 
         print(this +" " +  spriteType);
         // Set new sprite
         spriteRenderer.sprite = map.spriteTree.Get(spriteType);
     }
 
-    private string CheckPos (int xOff, int yOff) {
+    private bool CheckPos (int xOff, int yOff) {
         Vector2 pos = new Vector2(transform.position.x + xOff, transform.position.y + yOff);
         if (map.map.ContainsKey(pos) && map.map[pos].Count > 0) {
-            return "0";
+            return true;
         }
-        return "1";
+        return false;
     }
 
 
